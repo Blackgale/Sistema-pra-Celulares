e34f81e19743cafb777ec812e93eb416687071bc
Pedro <pedro.h.andrade@outlook.com>
2023-12-15 21:52:06 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Celular Nokia: ");
+Smartphone nokia = new Nokia(numero:"123456", modelo:"Mdelo 01", imei:"111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Aparelho Iphone: ");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "Modelo 02", imei: "222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
